30a6af756b3d4355838a328d3810c371435c23fc
srwebcl <contacto@srweb.cl>
2026-05-20 08:56:47 -0400

    feat: integrate Google Tag Manager GTM-P98QWP9G

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -64,6 +64,7 @@ import { AgeVerification } from "@/components/AgeVerification";
 
 import { CartProvider } from "@/context/CartContext";
 import { CartSidebar } from "@/components/CartSidebar";
+import Script from "next/script";
 
 export default async function LocaleLayout({
   children,
@@ -77,10 +78,29 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale} suppressHydrationWarning className="scroll-smooth">
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P98QWP9G');
+          `}
+        </Script>
+      </head>
       <body
         className={`${inter.variable} ${playfair.variable} antialiased bg-brand-dark text-white`}
         suppressHydrationWarning
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P98QWP9G"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <NextIntlClientProvider messages={messages}>
           <CartProvider>
             <Navbar />
